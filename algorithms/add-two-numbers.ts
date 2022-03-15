f1b40e40b50d5a4e0e1329d9d5efde1395a2b491
Federico Blancato <federico.blancato@infinitaslearning.com>
2022-03-15 21:45:30 +0100

    Optimize two numbers solution

--- algorithms/add-two-numbers.ts
+++ algorithms/add-two-numbers.ts
@@ -6,34 +6,28 @@ class ListNode {
     this.next = next === undefined ? null : next;
   }
 }
-
-const listToInt = (listValue: ListNode | null) => {
-  let cur = listValue;
-  let arrayValue = [];
-  if (listValue === null) return 0;
-  do {
-    typeof cur!.val === "number" && arrayValue.push(cur!.val);
-  } while (cur && cur.next && (cur = cur.next));
-  return parseInt(arrayValue.reverse().join(""));
-};
-const intToList = (integerValue: number) => {
-  const arrayValue = integerValue
-    .toLocaleString("fullwide", { useGrouping: false })
-    .split("")
-    .reverse()
-    .map((num) => parseInt(num, 10));
-  const head = new ListNode(arrayValue[0]);
-  let cur = head;
-  for (let i = 1; i <= arrayValue.length; i++) {
-    cur.next = new ListNode(arrayValue[i]);
-    cur = cur.next;
-  }
-  return head;
-};
 function addTwoNumbers(
   l1: ListNode | null,
   l2: ListNode | null
 ): ListNode | null {
-  const result = listToInt(l1) + listToInt(l2);
-  return intToList(result);
+  return add(l1, l2, 0);
+
+  function add(
+    l1: ListNode | null,
+    l2: ListNode | null,
+    carried: number
+  ): ListNode | null {
+    const value1 = (l1 && l1.val) || 0;
+    const value2 = (l2 && l2.val) || 0;
+    const sum = value1 + value2 + carried;
+    const carry = Math.floor(sum / 10);
+
+    if (l1 || l2 || carried) {
+      return {
+        val: sum % 10,
+        next: add(l1 && l1.next, l2 && l2.next, carry),
+      };
+    }
+    return null;
+  }
 }
