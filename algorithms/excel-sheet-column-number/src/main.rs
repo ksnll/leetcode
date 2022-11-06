94388c7ae2e3652255a36265c198e787b2bbbda9
Federico Blancato <federico.blancato@infinitaslearning.com>
2022-11-06 18:42:15 +0100

    Excel sheet column number

--- algorithms/excel-sheet-column-number/src/main.rs
+++ algorithms/excel-sheet-column-number/src/main.rs
@@ -1,11 +1,12 @@
-use std::collections::HashMap;
-
 struct Solution;
 impl Solution {
-impl Solution {
     pub fn title_to_number(column_title: String) -> i32 {
-        
-    }
+        let mut total = 0;
+        for c in column_title.chars() {
+            total = total * 26;
+            total = total + (c as usize - 64);
+        }
+        total as i32
     }
 }
 
@@ -13,7 +14,15 @@ impl Solution {
 mod test {
     #[test]
     fn title_to_number() {
-        assert_eq!(1, crate::Solution::title_to_number("A"));
-        assert_eq!(28, crate::Solution::title_to_number("AB"));
+        assert_eq!(1, crate::Solution::title_to_number("A".to_owned()));
+        assert_eq!(2, crate::Solution::title_to_number("B".to_owned()));
+        assert_eq!(26, crate::Solution::title_to_number("Z".to_owned()));
+        assert_eq!(28, crate::Solution::title_to_number("AB".to_owned()));
+        assert_eq!(29, crate::Solution::title_to_number("AC".to_owned()));
+        assert_eq!(701, crate::Solution::title_to_number("ZY".to_owned()));
     }
+}
+
+fn main() {
+    dbg!(Solution::title_to_number("ZY".to_owned()));
 }
